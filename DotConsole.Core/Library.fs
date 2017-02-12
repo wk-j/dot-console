22c28f98671c228720f732f68b71d9e992f32ed7
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-02-13 02:32:12 +0700

    Added NEW generator

--- DotConsole.Core/Library.fs
+++ DotConsole.Core/Library.fs
@@ -1,16 +1,67 @@
-﻿
-module DotConsole.Core.Library
+﻿module DotConsole.Core.Library
 
-type SlnName = string
-type ProjName = string
+type SolutionName = SolutionName of string
+type ProjectName = ProjectName of string
+type OutputDirectory = OutputDirectory of string
+type ProjectPath = ProjectPath of string
+type SolutionPath = SolutionPath of string
+type PackageName = PackageName of string
 
-type SlnVerb =
-      | New of SlnName
-      | Add of SlnName * ProjName
-      | Remove of SlnName * ProjName
+type GetValue = GetValue with
+    static member ($) (GetValue, (SolutionName v)) = v
+    static member ($) (GetValue, (ProjectName v)) = v
+    static member ($) (GetValue, (ProjectPath v)) = v
 
- 
-type New =
-      | Sln of SlnVerb 
-      | Proj 
+let inline value x : string = GetValue $ x
+
+type Language =
+      | FSharp
+      | CSharp
+
+type Project =
+      | Console of Language * ProjectName * OutputDirectory
+      | ClassLib of Language * ProjectName * OutputDirectory
+      | MsTest of Language * ProjectName * OutputDirectory
+      | XUnit of Language * ProjectName * OutputDirectory
+      | Web of  ProjectName * OutputDirectory
+      | Mvc of Language * ProjectName * OutputDirectory
+      | WebApi of ProjectName * OutputDirectory
+      | Sln of SolutionName * OutputDirectory
+
+type Reference =
+      | Project of ProjectPath
+      | Package of PackageName
+
+type ListType =
+      | Reference
+      | Package
+
+type Verb =
+      | New of Project
+      | Add of ProjectPath * Reference
+      | List of ProjectPath * ListType
+
+let langCmd lang =
+      match lang with
+      | FSharp -> "--lang F#"
+      | CSharp -> "--lange C#"
+
+let projectCmd project =
+      let gen types proj lang =
+            let path = value proj 
+            "{type} {lange} {name}"
+                  .Replace("{type}", types)
+                  .Replace("{lange}", langCmd lang)
+                  .Replace("{name}", path)
+
+      match project with
+      | Console (lang, proj, out) -> gen "console" proj lang
+      | ClassLib (lang, proj, out) -> gen "classlib" proj lang
+      | X -> ""
+
+let verbCmd verb =
+      match verb with
+      | New project -> sprintf "dotnet new %s" (projectCmd project)
+      | Add (path, reference) -> ""
+      | List (path, listType) -> ""
 
--- DotConsole.Tests/Test.fs
+++ DotConsole.Tests/Test.fs
@@ -1,11 +1,14 @@
-﻿namespace DotConsole.Tests
-open System
+﻿module DotConsole.Tests.Test
+
 open NUnit.Framework
+open FluentAssertions
 
-[<TestFixture>]
-type Test() = 
+open DotConsole.Core.Library
 
-    [<Test>]
-    member x.TestCase() =
-        Assert.IsTrue(true)
+[<Test>]
+let shouldCreateNewCmd() =
+      let project =  Console(FSharp, ProjectName("Hello"), OutputDirectory(""))
+      let verb = New(project)
 
+      let cmd = verbCmd verb
+      cmd.Should().Be("dotnet new console --lang F# Hello", "") |> ignore

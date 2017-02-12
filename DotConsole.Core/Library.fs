e3087cefd4c869d4e929eaf4f8f41fd637933b50
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-02-13 00:59:06 +0700

    Initial commit

---
+++ DotConsole.Core/Library.fs
@@ -0,0 +1,16 @@
+﻿
+module DotConsole.Core.Library
+
+type SlnName = string
+type ProjName = string
+
+type SlnVerb =
+      | New of SlnName
+      | Add of SlnName * ProjName
+      | Remove of SlnName * ProjName
+
+ 
+type New =
+      | Sln of SlnVerb 
+      | Proj 
+
---
+++ DotConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotConsole
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
